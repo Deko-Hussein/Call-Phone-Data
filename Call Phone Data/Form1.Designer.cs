c3864d0224dbbaa84e4a6c68930e8d3987690727
pc <deeqofoojaa@gmail.comgit config --global user.name>
2024-11-29 02:20:01 +0800

    Add project files.

---
+++ Call Phone Data/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Call_Phone_Data
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CreatObj = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pricelbl = new System.Windows.Forms.Label();
+            this.modelLbl = new System.Windows.Forms.Label();
+            this.brandl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(113, 102);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(226, 27);
+            this.textBox2.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Brand";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(104, 135);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 31);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Model";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(104, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 31);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 23);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 23);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 23);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Brand";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(6, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 23);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Model";
+            // 
+            // CreatObj
+            // 
+            this.CreatObj.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CreatObj.Font = new System.Drawing.Font("Sitka Display", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreatObj.Location = new System.Drawing.Point(80, 537);
+            this.CreatObj.Name = "CreatObj";
+            this.CreatObj.Size = new System.Drawing.Size(88, 51);
+            this.CreatObj.TabIndex = 19;
+            this.CreatObj.Text = "Create Object";
+            this.CreatObj.UseVisualStyleBackColor = false;
+            this.CreatObj.Click += new System.EventHandler(this.CreatObj_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ExitBtn.Font = new System.Drawing.Font("Sitka Display", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitBtn.Location = new System.Drawing.Point(388, 537);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(88, 51);
+            this.ExitBtn.TabIndex = 20;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = false;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(81, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(379, 217);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Cell Phone Data";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.pricelbl);
+            this.groupBox2.Controls.Add(this.modelLbl);
+            this.groupBox2.Controls.Add(this.brandl);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(76, 278);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(379, 187);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = " Get Call Phone Data";
+            // 
+            // pricelbl
+            // 
+            this.pricelbl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pricelbl.Location = new System.Drawing.Point(101, 118);
+            this.pricelbl.Name = "pricelbl";
+            this.pricelbl.Size = new System.Drawing.Size(176, 30);
+            this.pricelbl.TabIndex = 15;
+            // 
+            // modelLbl
+            // 
+            this.modelLbl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.modelLbl.Location = new System.Drawing.Point(101, 78);
+            this.modelLbl.Name = "modelLbl";
+            this.modelLbl.Size = new System.Drawing.Size(176, 30);
+            this.modelLbl.TabIndex = 14;
+            // 
+            // brandl
+            // 
+            this.brandl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.brandl.Location = new System.Drawing.Point(90, 37);
+            this.brandl.Name = "brandl";
+            this.brandl.Size = new System.Drawing.Size(176, 30);
+            this.brandl.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(982, 632);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.CreatObj);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button CreatObj;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label brandl;
+        private System.Windows.Forms.Label modelLbl;
+        private System.Windows.Forms.Label pricelbl;
+    }
+}
+
---
+++ Call Phone Data/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Call_Phone_Data
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void getPhonedata(CellPhone cell)
+        {
+            decimal price;
+            cell.brand = textBox1.Text;
+            cell.model = textBox2.Text;
+
+            if (decimal.TryParse(textBox3.Text, out price))
+            {
+                cell.price = price;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Price");
+            }
+        }
+
+        private void CreatObj_Click(object sender, EventArgs e)
+        {
+            CellPhone myphone = new CellPhone();
+            getPhonedata(myphone);
+            brandl.Text = myphone.brand;
+            modelLbl.Text = myphone.model;
+            pricelbl.Text = myphone.price.ToString();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class CellPhone
+    {
+        //Fields
+        private string _brand;  // the phone's brand
+        private string _model;  // The phone's model
+        private decimal _price; // Retail price
+
+        // Constructor
+        public CellPhone()
+        {
+            _brand = "";
+            _model = "";
+            _price = 0m;  // Initialize with decimal value
+        }
+
+        public string brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+
+        public string model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public decimal price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
